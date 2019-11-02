47ef652ac5b32293b614a8178ef7cee4f4c75acf
Aniruddh Kedlaya <akbazuka@Kedlayas-MacBook-Pro.local>
2019-11-01 16:26:54 -1000

    Added email passing functionality to registration page

--- register.php
+++ register.php
@@ -1,33 +1,26 @@
 <?php
 // Include config file
 require_once "config.php";
-
 // Define variables and initialize with empty values
-$username = $password = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = "";
-
+$username = $password = $confirm_password = $email = "";
+$username_err = $password_err = $confirm_password_err = $email_err = "";
 // Processing form data when form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
     // Validate username
     if (empty(trim($_POST["username"]))) {
         $username_err = "Please enter a username.";
     } else {
         // Prepare a select statement
         $sql = "SELECT id FROM users WHERE username = ?";
-
         if ($stmt = mysqli_prepare($link, $sql)) {
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_username);
-
             // Set parameters
             $param_username = trim($_POST["username"]);
-
             // Attempt to execute the prepared statement
             if (mysqli_stmt_execute($stmt)) {
                 /* store result */
                 mysqli_stmt_store_result($stmt);
-
                 if (mysqli_stmt_num_rows($stmt) == 1) {
                     $username_err = "This username is already taken.";
                 } else {
@@ -37,11 +30,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 echo "Oops! Something went wrong. Please try again later.";
             }
         }
-
         // Close statement
         mysqli_stmt_close($stmt);
     }
-
     // Validate password
     if (empty(trim($_POST["password"]))) {
         $password_err = "Please enter a password.";
@@ -50,7 +41,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $password = trim($_POST["password"]);
     }
-
     // Validate confirm password
     if (empty(trim($_POST["confirm_password"]))) {
         $confirm_password_err = "Please confirm password.";
@@ -61,19 +51,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 
+    //Validate Email
+    if (empty($_POST["email"])) {
+        $email_err = "Email is required";
+    } else {
+        $email = trim($_POST["email"]);
+        // check if e-mail address is well-formed
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $email_err = "Invalid email format";
+        }
+    }
+    
     // Check input errors before inserting in database
-    if (empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
-
+    if (empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($email_err)) {
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-
+        $sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
         if ($stmt = mysqli_prepare($link, $sql)) {
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
-
+            mysqli_stmt_bind_param($stmt, "sss", $param_username, $param_password, $param_email);
             // Set parameters
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            $param_email = $email;
             // Attempt to execute the prepared statement
             if (mysqli_stmt_execute($stmt)) {
                 // Redirect to login page
@@ -82,11 +81,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 echo "Something went wrong. Please try again later.";
             }
         }
-
         // Close statement
         mysqli_stmt_close($stmt);
     }
-
     // Close connection
     mysqli_close($link);
 }
@@ -106,7 +103,7 @@ include_once 'title.php';
                 font: 14px sans-serif; 
                 background-color: black;
             }
-            
+
             .outer {
                 display: table;
                 position: absolute;
@@ -115,12 +112,11 @@ include_once 'title.php';
                 height: 80%;
                 width: 100%;
             }
-            
+
             .middle {
                 display: table-cell;
                 vertical-align: middle;
             }
-
             .inner {
                 margin-left: auto;
                 margin-right: auto;
@@ -153,6 +149,11 @@ include_once 'title.php';
                                 <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
                                 <span class="help-block"><?php echo $confirm_password_err; ?></span>
                             </div>
+                            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                                <label>E-mail</label>
+                                <input type="email" name="email" class="form-control" value="<?php echo $email; ?>">
+                                <span class="help-block"><?php echo $email_err; ?></span>
+                            </div>
                             <div class="form-group">
                                 <input type="submit" class="btn btn-primary" value="Submit">
                                 <input type="reset" class="btn btn-default" value="Reset">
@@ -164,4 +165,3 @@ include_once 'title.php';
             </div>
         </body>
     </html>
-
