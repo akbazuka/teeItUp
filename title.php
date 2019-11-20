0b7fc3b796246aea82f89e4dadc644923eb1af26
Aniruddh Kedlaya <akbazuka@Kedlayas-MacBook-Pro.local>
2019-11-20 00:55:21 -1000

    Favicon added

--- title.php
+++ title.php
@@ -2,6 +2,7 @@
         <title>Tee It Up!</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel='shortcut icon' href='images/favicon.ico' type='image/x-icon'/> <!Adds favicon>
         <link href='https://fonts.googleapis.com/css?family=Paprika' rel='stylesheet'>
         <link rel="stylesheet" type="text/css" href="cssFiles/titleCSS.css">
     </head>
